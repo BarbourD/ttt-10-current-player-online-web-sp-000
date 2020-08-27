68f51559a568c1d05161265e44e1404dee30b66b
David Barbour <danceguy303@gmail.com>
2020-08-27 07:14:39 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,9 +12,9 @@ end
 
 
 def current_player(board)
-if turn_count(board) == 15 % 2
+if turn_count(board) == 5.odd?
   puts "X"
-else turn_count(board) == 4 % 2
+else turn_count(board) == 4.even?
   puts "O"
 end
 end
