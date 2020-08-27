345a02b2a2cd42f23c798dd4cad3774c6f878d0f
David Barbour <danceguy303@gmail.com>
2020-08-27 06:42:34 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,10 +2,9 @@ board1 = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
 board2 = ["O", " ", " ", " ", "X", " ", " ", "O", "X"]
 
 def turn_count(board1)
-counter = 1
+
 board1.each do
-  puts "This is turn #{counter}"
-  counter += 1
+
 end
 end
 
