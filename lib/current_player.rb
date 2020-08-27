a390d05177013cf35b16f5ddacdbe2ad11d29f3b
David Barbour <danceguy303@gmail.com>
2020-08-27 06:20:05 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,9 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
 counter = 0
-if board == "X" && board = "O"
+if board == "X" 
+  counter += 1
+else board == "O"
   counter += 1
 end
 board.each do |turns|
